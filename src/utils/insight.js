862488314c0a7abdf7279eba9adb43ed9595bf7d
jessvoiture <jessicascarr989@gmail.com>
2025-05-31 20:30:45 +0100

    update spelling

---
+++ src/utils/insight.js
@@ -0,0 +1,54 @@
+export const insight = [
+  {
+    type: "parentheses",
+    copy: "",
+  },
+  {
+    type: "apostrophe",
+    copy: "",
+  },
+  {
+    type: "comma",
+    copy: "",
+  },
+  {
+    type: "period",
+    copy: "",
+  },
+  {
+    type: "exclamation",
+    copy: "",
+  },
+  {
+    type: "question",
+    copy: "",
+  },
+  {
+    type: "slash",
+    copy: "",
+  },
+  {
+    type: "dash",
+    copy: "",
+  },
+  {
+    type: "ellipses",
+    copy: "",
+  },
+  {
+    type: "colons",
+    copy: "",
+  },
+  {
+    type: "quote",
+    copy: "",
+  },
+  {
+    type: "asterisk",
+    copy: "",
+  },
+  {
+    type: "ampersand",
+    copy: "",
+  },
+];
