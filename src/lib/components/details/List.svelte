7c6b861bbcad2bd214c0928f2aecdc7be0998fe8
jessvoiture <jessicascarr989@gmail.com>
2025-11-16 19:13:36 +0000

    add bit about # songs per year to about section

--- src/lib/components/details/List.svelte
+++ src/lib/components/details/List.svelte
@@ -103,14 +103,28 @@
         </div>
       </div>
     {:else if activeTab === "about"}
-      <div class="body-text">
-        The project explores the use of punctuation in titles of songs appearing
-        in the Billboard Hot 100 charts between 1958 and 2024. Historic
-        Billboard Hot 100 data was sourced from <a
-          href="https://github.com/utdata/rwd-billboard-data0"
-          target="_blank"
-          rel="noopener noreferrer">UT-Austin School of Journalism and Media</a
-        >. Songs only appear once, in the year they first entered the charts.
+      <div class="body-text-wrapper">
+        <div class="body-text">
+          Historic Billboard Hot 100 data was sourced from <a
+            href="https://github.com/utdata/rwd-billboard-data0"
+            target="_blank"
+            rel="noopener noreferrer"
+            >UT-Austin School of Journalism and Media</a
+          >. Songs only appear once, in the year they first entered the charts
+          in an effort to better capture the era of a song.
+        </div>
+
+        <div class="body-text">
+          The option to view the percent and number of songs per year is
+          available, as the number of new songs entering the chart varies quite
+          significantly throughout Billboard's history. Below is a chart of the
+          number of new charting songs per year.
+        </div>
+
+        <img
+          src="/count_songs_per_year.png"
+          alt="A vertical bar chart showing the number of new songs entering the Billboard Hot 100 charts each year from 1958 to 2024. The trend has a U-shape: the number of songs peaks in the 1960s with over 700 new songs charting in the Hot 100, then declines, reaching a low of 288 in 2001. Since then early 2000s the number of new charting songs has generally increased. In 2024 there were 629 new songs entering the charts."
+        />
       </div>
     {/if}
   </div>
@@ -180,5 +194,11 @@
 
   .tab:hover {
     background-color: #f6f6f6;
+  }
+
+  .body-text-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
   }
 </style>
