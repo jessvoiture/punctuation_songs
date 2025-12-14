d8c66305347f8d5c57b32a2e7f578b7b2746afc4
jessvoiture <jessicascarr989@gmail.com>
2025-12-14 21:41:58 +0000

    about section

---
+++ src/lib/components/details/About.svelte
@@ -0,0 +1,38 @@
+<div class="body-text-wrapper">
+  <div class="body-text">
+    Historic Billboard Hot 100 data was sourced from <a
+      href="https://github.com/utdata/rwd-billboard-data0"
+      target="_blank"
+      rel="noopener noreferrer">UT-Austin School of Journalism and Media</a
+    >. Songs only appear once, in the year they first entered the charts in an
+    effort to better capture the era of a song.
+  </div>
+
+  <div class="body-text">
+    Using the year a song first debuts on the chart is not always reflective of
+    the year it was published. Many songs have found chart success years after
+    release. For example, Running Up That Hill (A Deal with God) by Kate Bush
+    released in 1985 didn't chart on the Hot 100 until 2022, after being
+    featured in Stranger Things.
+  </div>
+
+  <div class="body-text">
+    The option to view the percent and number of songs per year is available, as
+    the number of new songs entering the chart varies quite significantly
+    throughout Billboard's history. Below is a chart of the number of new
+    charting songs per year.
+  </div>
+
+  <img
+    src="/count_songs_per_year.png"
+    alt="A vertical bar chart showing the number of new songs entering the Billboard Hot 100 charts each year from 1958 to 2024. The trend has a U-shape: the number of songs peaks in the 1960s with over 700 new songs charting in the Hot 100, then declines, reaching a low of 288 in 2001. Since then early 2000s the number of new charting songs has generally increased. In 2024 there were 629 new songs entering the charts."
+  />
+</div>
+
+<style>
+  .body-text-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+</style>
