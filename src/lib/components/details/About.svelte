d0f7c67cf5756dd0991ae209d3e719cba072bbaf
jessvoiture <jessicascarr989@gmail.com>
2026-01-14 21:55:14 +0000

    update about

--- src/lib/components/details/About.svelte
+++ src/lib/components/details/About.svelte
@@ -10,10 +10,7 @@
 
   <div class="body-text">
     Using the year a song first debuts on the chart is not always reflective of
-    the year it was published. Many songs have found chart success years after
-    release. For example, Running Up That Hill (A Deal with God) by Kate Bush
-    released in 1985 didn't chart on the Hot 100 until 2022, after being
-    featured in Stranger Things.
+    the year it was published. Songs may find chart success years after release.
   </div>
 
   <div class="body-text">
@@ -27,6 +24,20 @@
     src="/count_songs_per_year.png"
     alt="A vertical bar chart showing the number of new songs entering the Billboard Hot 100 charts each year from 1958 to 2024. The trend has a U-shape: the number of songs peaks in the 1960s with over 700 new songs charting in the Hot 100, then declines, reaching a low of 288 in 2001. Since then early 2000s the number of new charting songs has generally increased. In 2024 there were 629 new songs entering the charts."
   />
+
+  <div class="body-text">
+    This project was built with svelte. All data analysis was done with R. You
+    can find the code and data on <a
+      href="https://github.com/jessvoiture/punctuation_songs"
+      target="_blank">GitHub</a
+    >. It was created by me,
+    <a href="https://jessvoiture.github.io/home/" target="_blank">Jess Carr</a>.
+    If you have feedback or questions, feel free to send me a message at
+    <a href="mailto:someone@example.com">jessicascarr989@gmail.com</a>
+    . Thank you so much for visiting!
+  </div>
+
+  <div class="body-text">Last updated 14 Jan 2026.</div>
 </div>
 
 <style>
